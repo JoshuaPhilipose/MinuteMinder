3e1ce58b141156c1aeeb010c7865c92d96cf4e81
JoshuaPhilipose <Joshphilipose@gmail.com>
2017-05-28 15:05:07 -0400

    Initial commit + Basic Button Notification

---
+++ app/src/main/java/com/example/joshua/minuteminder/SectionsPagerAdapter.java
@@ -0,0 +1,46 @@
+package com.example.joshua.minuteminder;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by Joshua on 5/28/2017.
+ */
+/**
+ * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+
+
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+        return MainActivity.PlaceholderFragment.newInstance(position + 1);
+    }
+
+    @Override
+    public int getCount() {
+        // Show 3 total pages.
+        return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "Minders";
+            case 1:
+                return "Alarms";
+            case 2:
+                return "SECTION 3";
+        }
+        return null;
+    }
+}
