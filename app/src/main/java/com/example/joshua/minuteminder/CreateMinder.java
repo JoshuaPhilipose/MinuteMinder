28555a4311468c12699b4833d2c40dc51eb7375e
JoshuaPhilipose <Joshphilipose@gmail.com>
2017-07-17 23:54:35 -0400

    Toggle button UI working properly, added documentation

--- app/src/main/java/com/example/joshua/minuteminder/CreateMinder.java
+++ app/src/main/java/com/example/joshua/minuteminder/CreateMinder.java
@@ -8,15 +8,11 @@ import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CompoundButton;
 import android.widget.EditText;
-import android.widget.ToggleButton;
 
 import java.util.Calendar;
 import java.util.Timer;
@@ -24,47 +20,13 @@ import java.util.TimerTask;
 
 public class CreateMinder extends AppCompatActivity {
 
-    private int milliseconds;
+    private Minder m;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_minder);
 
-
-        // TOGGLE SWITCH NOTIFICATION - FOR NOW LETS LEAVE IT ALWAYS ON
-//        final ToggleButton toggleSwitch = (ToggleButton) findViewById(R.id.toggleButton);
-//        toggleSwitch.setChecked(false);
-//        toggleSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-//                if (isChecked) {
-////                    minder1.setIsActive(true);
-//                } else {
-////                    minder1.setIsActive(false);
-//                }
-//            }
-//        });
-
-//        //SUBMITTING FREQUENCY CHANGE
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.floatingActionButton);
-//        final EditText input = (EditText) findViewById(R.id.textView);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                String inputText = input.getText().toString();
-//                if (isIntegerString(inputText) && !inputText.equals("")) {
-//                    milliseconds = Integer.parseInt(inputText) * 1000;
-////                    minder1.setFrequency(milliseconds);
-////                    minder1.deployMinder();
-//                    Snackbar.make(view, "Frequency set to every " + (milliseconds/1000) + " seconds.", Snackbar.LENGTH_LONG)
-//                            .setAction("Action", null).show();
-//                } else {
-//                    Snackbar.make(view, "Please enter a numeric value!", Snackbar.LENGTH_LONG)
-//                            .setAction("Action", null).show();
-//                }
-//            }
-//        });
-    //End of onCreate()
         final Context context = this;
 
         Button createMinder = (Button) findViewById(R.id.createMinder);
@@ -73,14 +35,16 @@ public class CreateMinder extends AppCompatActivity {
             public void onClick(View view) {
                 final EditText minderNameEditText = (EditText) findViewById(R.id.minderName);
                 final EditText minderFrequencyEditText = (EditText) findViewById(R.id.minderFrequency);
-                String minderName = minderNameEditText.getText().toString();
+                String minderName = "" + minderNameEditText.getText().toString();
                 int minderFrequency = Integer.parseInt(minderFrequencyEditText.getText().toString());
-                MainActivity.addMinder(new Minder(minderName, minderFrequency));
+                m = new Minder(minderName, minderFrequency);
+                MainActivity.setMinder(m);
 
                 Intent intent = new Intent(context, MainActivity.class);
                 startActivity(intent);
             }
         });
+        //End of onCreate()
     }
 
     //Checks if the string is composed of only integers
@@ -91,60 +55,5 @@ public class CreateMinder extends AppCompatActivity {
         } catch (Exception e) {
             return false;
         }
-    }
-
-    //Creates concrete class for abstract TimerTask
-    class NotificationTimerTask extends TimerTask {
-        public void run() {
-            createNotification(getApplicationContext(), getCurrTime());
-
-        }
-    }
-
-    private void createNotification(Context context, String message) {
-
-        long when = System.currentTimeMillis();
-        String appName = context.getResources().getString(R.string.app_name);
-        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        Notification notification;
-        PendingIntent contentIntent = PendingIntent.getActivity(context, 0, new Intent(context, CreateMinder.class), 0);
-        Uri uri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(
-                context);
-        notification = builder.setContentIntent(contentIntent)
-                .setSmallIcon(R.drawable.clock)
-                .setTicker(appName)
-                .setWhen(0)
-                .setAutoCancel(true)
-                .setContentTitle(appName)
-                .setContentText(message)
-                .setSound(uri)
-                .build();
-        notificationManager.notify((int) when, notification);
-    }
-
-    public void deployMinder(Minder m) {
-        if (m.getIsActive()) {
-            final NotificationTimerTask myTask = new NotificationTimerTask();
-            final Timer myTimer = new Timer();
-
-            //Scheduling parameters are task, time till start, and time upon which to repeat
-            //30000 milliseconds is 30 seconds
-            myTimer.schedule(myTask, m.getFrequency(), m.getFrequency());
-        }
-    }
-
-    //Returns current time in a formatted string
-    private String getCurrTime() {
-        Calendar calendar = Calendar.getInstance();
-        int hour = calendar.get(Calendar.HOUR);
-        int minute = calendar.get(Calendar.MINUTE);
-        //Ensuring minute is always double digit
-        String min = minute > 9 ? "" + minute : "0" + minute;
-        //Ternary expression to set AM/PM
-        String am_pm = (int) calendar.get(Calendar.AM_PM) == Calendar.AM ? " AM" : " PM";
-        String returner = "The time is ";
-        returner += hour + ":" + min + am_pm;
-        return returner;
     }
 }
--- app/src/main/java/com/example/joshua/minuteminder/MainActivity.java
+++ app/src/main/java/com/example/joshua/minuteminder/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.joshua.minuteminder;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.support.v4.app.Fragment;
@@ -17,6 +24,9 @@ import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import static com.example.joshua.minuteminder.R.id.container;
 
@@ -25,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
     private SectionsPagerAdapter mSectionsPagerAdapter;
     private ViewPager mViewPager;
     private static ArrayList<Minder> minderArrayList = new ArrayList<Minder>(10);
+    private static Minder minder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
+
     }
 
     @Override
@@ -64,17 +76,69 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public static void addMinder(Minder minder) {
+    public void addMinder() {
         minderArrayList.add(minder);
+        deployMinder(minder);
+    }
+
+    public void deployMinder(Minder m) {
+        if (m.getIsActive()) {
+            NotificationTimerTask myTask = new NotificationTimerTask();
+            Timer myTimer = new Timer();
+
+            //Scheduling parameters are task, time till start, and time upon which to repeat
+            //30000 milliseconds is 30 seconds
+            myTimer.schedule(myTask, m.getFrequency(), m.getFrequency());
+        }
+    }
+
+    //Creates concrete class for abstract TimerTask
+    class NotificationTimerTask extends TimerTask {
+        public void run() {
+            createNotification(getApplicationContext(), getCurrTime());
+
+        }
+    }
+
+    private void createNotification(Context context, String message) {
+
+        long when = System.currentTimeMillis();
+        String appName = context.getResources().getString(R.string.app_name);
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        PendingIntent contentIntent = PendingIntent.getActivity(context, 0, new Intent(context, CreateMinder.class), 0);
+        Uri uri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(
+                context);
+        Notification notification = builder.setContentIntent(contentIntent)
+                .setSmallIcon(R.drawable.clock)
+                .setTicker(appName)
+                .setWhen(0)
+                .setAutoCancel(true)
+                .setContentTitle(appName)
+                .setContentText(message)
+                .setSound(uri)
+                .build();
+        notificationManager.notify((int) when, notification);
+    }
+
+    //Returns current time in a formatted string
+    private String getCurrTime() {
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR);
+        int minute = calendar.get(Calendar.MINUTE);
+        //Ensuring minute is always double digit
+        String min = minute > 9 ? "" + minute : "0" + minute;
+        //Ternary expression to set AM/PM
+        String am_pm = (int) calendar.get(Calendar.AM_PM) == Calendar.AM ? " AM" : " PM";
+        String returner = "The time is ";
+        returner += hour + ":" + min + am_pm;
+        return returner;
     }
 
     public static class PlaceholderFragment extends Fragment {
-
         private static final String ARG_SECTION_NUMBER = "section_number";
-
         public PlaceholderFragment() {
         }
-
         public static PlaceholderFragment newInstance(int sectionNumber) {
             PlaceholderFragment fragment = new PlaceholderFragment();
             Bundle args = new Bundle();
@@ -105,12 +169,20 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
 
-            ListView minderList = (ListView) rootView.findViewById(R.id.minderList);
-            ArrayAdapter<Minder> arrayAdapter = new ArrayAdapter<Minder>(getContext(), android.R.layout.simple_list_item_1, minderArrayList );
-
-            minderList.setAdapter(arrayAdapter);
+//            ListView minderList = (ListView) rootView.findViewById(R.id.minderList);
+//            ArrayAdapter<Minder> arrayAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, minderArrayList);
+//
+//            minderList.setAdapter(arrayAdapter);
 
             return rootView;
         }
+    }
+
+    public Minder getMinder() {
+        return minder;
+    }
+
+    public static void setMinder(Minder m) {
+        minder = m;
     }
 }
